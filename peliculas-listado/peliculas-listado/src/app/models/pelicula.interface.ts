37d80e067c36b335d1c07b692f5ea1e34796d47b
Ricardo Mejías Dorado <rmejias@AULAS.TRIANA>
2019-10-08 14:29:44 +0200

    Peliculas en construccion

---
+++ peliculas-listado/peliculas-listado/src/app/models/pelicula.interface.ts
@@ -0,0 +1,24 @@
+declare module namespace {
+
+    export interface Result {
+        popularity: number;
+        vote_count: number;
+        video: boolean;
+        poster_path: string;
+        id: number;
+        adult: boolean;
+        backdrop_path: string;
+        original_language: string;
+        original_title: string;
+        genre_ids: number[];
+        title: string;
+        vote_average: number;
+        overview: string;
+        release_date: string;
+    }
+
+    export interface RootObject {
+        results: Result[];
+    }
+
+}
